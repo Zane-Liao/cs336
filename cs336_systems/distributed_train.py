a3e3c0168c16c83d05daf8bfb1ba263c73521a8f
Zhengqiang Liao <lzq666amn@gmail.com>
2025-10-11 12:14:28 +0800

    --

--- cs336_systems/distributed_train.py
+++ cs336_systems/distributed_train.py
@@ -5,7 +5,7 @@ OS: Ubuntu Linux
 GPU: GPU PCIE X 4
 device: GPU0 GPU1 GPU2 GPU3
 Shell: bash
-Improvement: torch.compile() FlashAttention DDP
----
+Improvement: torch.compile() FlashAttention DDP OptimizerStateShare
+Our Impl: Use torch.nn.parallel.DistributedDataParallel
 """
 
